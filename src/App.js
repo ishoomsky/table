2052308d54bf9945fbaad98b1a47a72a62975f46
Ivan Shumski <ishoomsky@gmail.com>
2021-07-01 13:00:24 +0300

    style code, update json

--- src/App.js
+++ src/App.js
@@ -3,20 +3,12 @@ import AppTable from "./components/AppTable";
 import { initLS } from "./functions/localStorageFunctions";
 import initialData from "./initData.json";
 
-
-const lsKey = "app-data";
-
 function App() {
   useEffect(() => {
-    initLS(lsKey, initialData);
-    // console.log(initialData);
+    initLS(initialData);
   }, []);
 
-  return (
-    <>
-      <AppTable />
-    </>
-  );
+  return <AppTable />;
 }
 
 export default App;
