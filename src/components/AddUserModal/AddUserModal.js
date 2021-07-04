83540ff2ac03ac3e60f75fdde20fc09272744073
Ivan Shumski <ishoomsky@gmail.com>
2021-07-05 00:19:36 +0300

    add formik

--- src/components/AddUserModal/AddUserModal.js
+++ src/components/AddUserModal/AddUserModal.js
@@ -1,5 +1,8 @@
 import React, { useState } from "react";
 import PropTypes from "prop-types";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+
 import {
   Modal,
   Form,
@@ -15,44 +18,59 @@ import {
 
 import { generateRandomId } from "../../functions/generateRandomId";
 
+const validationSchema = Yup.object().shape({
+  name: Yup.string().required("Please enter user's name").label("name"),
+  group: Yup.string().required("Please choose user's group").label("group"),
+  balance: Yup.number()
+    .required("Please enter user's balance")
+    .label("balance"),
+  status: Yup.string().required("Please enter user's status").label("status"),
+  note: Yup.string().label("note"),
+});
+
 export default function AddUserModal({
   isModalOpen,
   handleAddUser,
   setModalOpen,
   userGroups,
 }) {
-  const [isLoading, setIsLoading] = useState(false); //0 - normal view, 1 - loading view
-  const [name, setName] = useState("");
-  const [group, setGroup] = useState("placeholder-item");
-  const [balance, setBalance] = useState(0);
-  const [status, setStatus] = useState("");
-  const [note, setNote] = useState("");
+  const formik = useFormik({
+    initialValues: {
+      name: "",
+      group: "",
+      balance: "",
+      status: "",
+      note: "",
+    },
+    onSubmit: (values) => {
+      console.log(values);
+    },
+    validationSchema: validationSchema,
+  });
+
+  const [isLoading, setIsLoading] = useState(false);
 
   const resetModalForm = () => {
-    setName("");
-    setGroup("placeholder-item");
-    setBalance(0);
-    setStatus("");
-    setNote("");
+    formik.handleReset();
   };
 
   const handleSubmit = () => {
-    const newUser = {
-      id: generateRandomId(),
-      name: name,
-      group: group,
-      balance: balance,
-      status: status === "radio-1" ? true : false,
-      note: note,
-    };
-
-    setIsLoading(true);
-    setTimeout(() => {
-      handleAddUser(newUser);
-      setIsLoading(false);
-      resetModalForm();
-      setModalOpen(false);
-    }, 200);
+    const formikValues = formik.handleSubmit();
+    // const newUser = {
+    //   id: generateRandomId(),
+    //   name: name,
+    //   group: group,
+    //   balance: balance,
+    //   status: status === "radio-1" ? true : false,
+    //   note: note,
+    // };
+    // setIsLoading(true);
+    // setTimeout(() => {
+    //   handleAddUser(newUser);
+    //   setIsLoading(false);
+    //   resetModalForm();
+    //   setModalOpen(false);
+    // }, 200);
   };
 
   const handleCancel = () => {
@@ -73,66 +91,85 @@ export default function AddUserModal({
     >
       <Loading active={isLoading} />
       <Form>
-        <TextInput
-          onChange={(e) => setName(e.target.value)}
-          value={name}
-          id="user-name"
-          labelText="Enter name"
-          placeholder="John Doe"
-          style={{ marginBottom: "1rem" }}
-          data-modal-primary-focus
-          autoComplete={"off"}
-        />
-
-        <Select
-          onChange={(e) => setGroup(e.target.value)}
-          value={group}
-          id="user-group"
-          labelText="Choose an group"
-          style={{ marginBottom: "1rem" }}
-          autoComplete={"off"}
-        >
-          <SelectItem
-            disabled
-            hidden
-            value="placeholder-item"
-            text="Choose an group"
+        <div style={{ marginBottom: "15px" }}>
+          {console.log(formik.touched)}
+          <TextInput
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.name}
+            name={"name"}
+            invalid={!!formik.errors.name && !!formik.touched.name}
+            invalidText={formik.errors.name}
+            id="user-name"
+            labelText="Enter name"
+            placeholder="John Doe"
+            autoComplete={"off"}
           />
-          {userGroups?.map((group) => (
-            <SelectItem key={group} value={group} text={group} />
-          ))}
-        </Select>
-
-        <TextInput
-          onChange={(e) => setBalance(e.target.value)}
-          value={balance}
-          id="user-balance"
-          labelText="Enter balance, BYN"
-          placeholder="0"
-          autoComplete={"off"}
-        />
-        <FormGroup legendText="Choose user's status">
-          <RadioButtonGroup
-            onChange={(value) => setStatus(value)}
-            valueSelected={status}
-            name="user-status"
+        </div>
+        <div style={{ marginBottom: "15px" }}>
+          <Select
+            onChange={formik.handleChange}
+            value={formik.values.group}
+            name={"group"}
+            invalid={!!formik.errors.group && !!formik.touched.group}
+            invalidText={formik.errors.group}
+            id="user-group"
+            labelText="Choose an user's group"
+            autoComplete={"off"}
           >
-            <RadioButton labelText="Active" value="radio-1" name="radio-1" />
-            <RadioButton
-              labelText="Not active"
-              value="radio-2"
-              name="radio-2"
-            />
-          </RadioButtonGroup>
-        </FormGroup>
+            <SelectItem disabled hidden value="" text="" />
+            {userGroups?.map((group) => (
+              <SelectItem key={group} value={group} text={group} />
+            ))}
+          </Select>
+        </div>
+        <div style={{ marginBottom: "15px" }}>
+          <TextInput
+            onChange={formik.handleChange}
+            value={formik.values.balance}
+            name={"balance"}
+            invalid={!!formik.errors.balance && !!formik.touched.balance}
+            invalidText={formik.errors.balance}
+            id="user-balance"
+            labelText="Enter balance, BYN"
+            placeholder="0"
+            autoComplete={"off"}
+          />
+        </div>
 
-        <TextArea
-          onChange={(e) => setNote(e.target.value)}
-          value={note}
-          labelText={"Note"}
-          rows={2}
-          autoComplete={"off"}
-        />
+        <div style={{ marginBottom: "15px" }}>
+          <FormGroup legendText="Choose user's status">
+            <div style={{ padding: "5px", border: "2px solid #da1e28" }}>
+              <RadioButtonGroup
+                onChange={(value) => {
+                  formik.setFieldValue("status", value);
+                }}
+                valueSelected={formik.values.status}
+                name="status"
+              >
+                <RadioButton labelText="Active" value="Active" name="radio-1" />
+                <RadioButton
+                  labelText="Not active"
+                  value="Not active"
+                  name="radio-2"
+                />
+              </RadioButtonGroup>
+            </div>
+          </FormGroup>
+        </div>
+        <div style={{ marginBottom: "15px" }}>
+          <TextArea
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.note}
+            name={"note"}
+            // invalid={!!formik.errors.note && !!formik.touched.note}
+            // invalidText={formik.errors.note}
+            labelText={"Note"}
+            rows={2}
+            autoComplete={"off"}
+          />
+        </div>
       </Form>
     </Modal>
   );
@@ -145,7 +182,7 @@ AddUserModal.defaultProps = {
 };
 
 AddUserModal.propTypes = {
-  open: PropTypes.bool.isRequired,
+  open: PropTypes.bool,
   userGroups: PropTypes.array,
   handleAddUser: PropTypes.func,
   setModalOpen: PropTypes.func,
