250201168e529f582c220f1022f31c9b39c4c254
Ivan Shumski <ishoomsky@gmail.com>
2021-07-08 15:18:20 +0300

    add delete feature

---
+++ src/components/DeleteUserModal/DeleteUserModal.js
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import { Modal, Loading } from "carbon-components-react";
+
+import { generateRandomId } from "../../functions/generateRandomId";
+
+export default function DeleteModal({
+  modalOpen,
+  setModalOpen,
+  handleDeleteUser,
+  setNotification,
+  currentUser,
+}) {
+  const [modalLoading, setModalLoading] = useState(false);
+  const notification = {
+    kind: "warning",
+    title: `User ${currentUser?.name} was deleted`,
+    id: generateRandomId(),
+  };
+  const handleSubmit = (id) => {
+    setModalLoading(true);
+    setTimeout(() => {
+      handleDeleteUser(id);
+      setModalLoading(false);
+      setModalOpen(false);
+      setNotification(notification);
+    }, 200);
+  };
+
+  const renderModal = (
+    <Modal
+      open={modalOpen}
+      danger={true}
+      modalHeading={`Are you sure you want to delete ${currentUser?.name}`}
+      primaryButtonText={"Delete user"}
+      secondaryButtonText={"Cancel"}
+      onRequestSubmit={() => handleSubmit(currentUser?.id)}
+      onSecondarySubmit={() => setModalOpen(false)}
+      onRequestClose={() => setModalOpen(false)}
+    >
+      <Loading active={modalLoading} />
+    </Modal>
+  );
+
+  return renderModal;
+}
+
+DeleteModal.propTypes = {
+  modalOpen: PropTypes.bool.isRequired,
+  setModalOpen: PropTypes.func.isRequired,
+  currentUser: PropTypes.object,
+  handleDeleteUser: PropTypes.func.isRequired,
+  setNotification: PropTypes.func.isRequired,
+};
