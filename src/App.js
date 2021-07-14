a8ec4d1b0a902a1074b03c2e76abf12af15dab55
Ivan Shumski <ishoomsky@gmail.com>
2021-07-14 09:54:34 +0300

    clear code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
 import React, { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 
 import { usersFetch } from "./store/reducers/usersReducer";
 import { userGroupsFetch } from "./store/reducers/userGroupsReducer";
 
 import AppTable from "./components/AppTable";
-// import { initLS } from "./functions/localStorageFunctions";
-// import initialData from "./initData.json";
 
 function App() {
   const dispatch = useDispatch();
-  // const wut = useSelector((state) => state);
-  // console.log(wut);
-  useEffect(() => {
-    console.log("run");
-    dispatch(usersFetch());
-    // initLS(initialData);
-  }, []);
 
   useEffect(() => {
-    console.log("user groups");
+    dispatch(usersFetch());
     dispatch(userGroupsFetch());
   }, []);
 
