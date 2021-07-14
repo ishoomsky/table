668f22e928bce9757d4ca4c553757527dddc3d66
Ivan Shumski <ishoomsky@gmail.com>
2021-07-14 10:01:59 +0300

    remove unused code

--- src/functions/localStorageFunctions.js
+++ src/functions/localStorageFunctions.js
@@ -1,12 +1,3 @@
-const lsKey = "app-data-users";
-
-// export const initLS = (_data, localStorageKey) => {
-//   const data = JSON.stringify(_data);
-//   if (localStorage.getItem(localStorageKey) === null) {
-//     localStorage.setItem(localStorageKey, data);
-//   }
-// };
-
 export const getLS = (localStorageKey) => {
   return JSON.parse(localStorage.getItem(localStorageKey));
 };
