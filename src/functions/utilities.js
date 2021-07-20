a91702af30051c9ee5c43c0f7d7a255674ca064a
Ivan Shumski <ishoomsky@gmail.com>
2021-07-20 12:19:38 +0300

    combine functions

--- src/functions/utilities.js
+++ src/functions/utilities.js
@@ -1,3 +1,15 @@
 export const isEmptyObject = (obj) => {
   return obj && Object.keys(obj).length === 0 && obj.constructor === Object;
 };
+
+export const generateRandomId = () => {
+  return Math.random().toString(36).substr(2, 9);
+};
+
+export const getWindowDimensions = () => {
+  const { innerWidth: width, innerHeight: height } = window;
+  return {
+    width,
+    height,
+  };
+}
